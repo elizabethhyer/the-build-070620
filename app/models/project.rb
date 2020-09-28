ecc873e1cf3ea09526fd1050b3bc9ea12f957938
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-09-28 18:11:44 -0400

    Adds a validation to the project clas

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,6 @@ class Project < ApplicationRecord
     belongs_to :discipline 
     has_many :notes
     has_many :users, through: :notes
+    validates :name, presence: true
     validates :technique_used, presence:true 
 end
