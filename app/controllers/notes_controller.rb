2195ccf7740e11e9ea69ce22606876fb94d0e4f3
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-09-29 14:36:26 -0400

    Adds create and update controller actions for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,19 +1,35 @@
 class NotesController < ApplicationController
 
-    def show 
-        @note = Note.find_by(id: params[:id])
+    def index 
+        @notes = Note.all
     end 
 
     def new
+        @note = Note.new
     end 
 
     def create
+        @note = Note.new(note_params)
+        if @note.save
+            redirect_to somewhere
+        else
+            @errors = @project.errors.full_messages
+            render :new 
+        end 
     end 
 
     def edit 
+        @note = Note.find_by(id: params[:id])
     end 
 
     def update
+        @note = Note.find_by(id: params[:id])
+        if @note.update(note_params)
+            redirect_to somewhere
+        else
+            @errors = @project.errors.full_messages
+            render :edit 
+        end 
     end 
 
     private 
