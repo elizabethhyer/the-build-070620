2219f819eafa84539fa2e2666bbfd5ddc62b635f
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-09-30 19:07:54 -0400

    Adds login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController < ApplicationController
     end 
 
     def create 
-
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to projects_path
+        else 
+            @errors = @user.errors.full_messages
+            render :new 
+        end 
     end 
 
     def logout
