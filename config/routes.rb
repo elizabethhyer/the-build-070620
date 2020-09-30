f2dfa87f769571972c2f8a448b988065f00fc113
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-09-30 13:43:26 -0400

    Adds a signup form for users

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 
   get 'about', to: 'static#about'
   root 'static#home'
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
